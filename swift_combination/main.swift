f43be360197b6273a380a648fe54b8fdc8357716
Keita Okamoto <ok@aaa.email.ne.jp>
2014-11-19 10:02:58 +0900

    Initial Commit

---
+++ swift_combination/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift_combination
+//
+//  Created by Keita Okamoto on 2014/11/19.
+//  Copyright (c) 2014年 Keita Okamoto. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
