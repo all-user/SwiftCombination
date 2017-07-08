55f0a9d276664e70fbd02d95ac66b9cbdcf0daab
Keita Okamoto <ok@aaa.email.ne.jp>
2017-07-08 14:26:13 +0900

    Add tests to XCTMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,12 @@ import XCTest
 @testable import SwiftCombinationTests
 
 XCTMain([
+  testCase(ArrayCombinationTests.allTests),
+  testCase(ArrayPermutationTests.allTests),
+  testCase(ArrayRepeatedCombinationTests.allTests),
+  testCase(ArrayRepeatedPermutationTests.allTests),
   testCase(GlobalCombinationTests.allTests),
+  testCase(GlobalPermutationTests.allTests),
+  testCase(GlobalRepeatedCombinationTests.allTests),
+  testCase(GlobalRepeatedPermutationTests.allTests),
 ])
